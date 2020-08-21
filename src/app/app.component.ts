c571c96ba441c14249cd116386bbc9949d9f0de3
olcay <olcayusta02@gmail.com>
2020-08-21 06:44:00 +0300

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'sinemalar1-app';
+
+  links = ['Ana Sayfa', 'Vizyondaki Filmler', 'Yakında'];
+  activeLink = this.links[0];
+
+  visionItems = [
+    {img: 'https://img01.imgsinemalar.com/images/afis_dev/t/the-lodge-1582527821.jpg', title: 'Mürit'},
+    {img: 'https://img01.imgsinemalar.com/images/afis_dev/t/the-painted-bird-1489304616.jpg', title: 'Boyalı Kuş'},
+    {img: 'https://img01.imgsinemalar.com/images/afis_dev/p/portal-1573814250.jpg', title: 'Geçit'},
+    {img: 'https://img01.imgsinemalar.com/images/afis_dev/k/kahramanlar-1597150317.jpg', title: 'Kahramanlar'},
+    {img: 'https://img01.imgsinemalar.com/images/afis_dev/t/the-lost-lion-kingdom-1557874558.jpg', title: 'Aslan Krallığı'},
+    {img: 'https://img01.imgsinemalar.com/images/afis_dev/4/47-meters-down-the-next-chapter-1556192992.jpg', title: '47 Meters Down: Uncaged'},
+  ]
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatIconModule } from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,15 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
